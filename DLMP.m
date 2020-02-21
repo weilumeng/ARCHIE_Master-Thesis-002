1e533195a1fe16ed5a9a6f165eb3412511be99e1
gduessel <60701948+gduessel@users.noreply.github.com>
2020-02-21 18:45:05 +0100

    Reverted some changes

--- DLMP.m
+++ DLMP.m
@@ -1,16 +1,4 @@
 % Master Thesis
 % Creating DC OPF first
 
-%Lets try to add some commands 
-
-%to check if the git module is working
-
-x=1
-x=x*3;
-x=7*x;
-x=x^2
-
-tz=9;
-u=1;
-
-x=x*89
+%Reverted using Git
