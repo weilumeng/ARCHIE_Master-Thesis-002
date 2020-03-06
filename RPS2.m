357b3420942cb157454b62dcd20ac4877fac2fb5
gduessel <60701948+gduessel@users.noreply.github.com>
2020-03-06 19:54:54 +0100

    newest changes

---
+++ RPS2.m
@@ -0,0 +1,73 @@
+        %Program to calualte locational marginal price for the problem
+        %given in Sahidapur book page 395
+        %This program calculates the LMP using DCOPf model
+        %Program developed by Dr.Arunachalam Sundaram
+        clear
+        basemva = 100;  accuracy = 0.001; maxiter = 50;
+
+
+
+        gexdata=[1 10 600 1 0 600
+                 2 20 200 3 0 300 ];
+        dexdata=[1 25 320 3 0 320
+                 2 35 180 2 0 180];
+     
+        %        No  code Mag.    Degree  MW    Mvar  MW  Mvar Qmin Qmax     Mvar
+        busdata=[1   1    1.0    0.0     0.0   0.0    0     0.0    0   0       0
+                 2   0    1.0    0.0     180   0.0    0.0   0.0    0   0       0
+                 3   2    1.0    0.0     320   0.0    0.0   0.0    0   0       0
+                ];
+        linedata =[...
+                 1            2      0       0.25         0     1          200
+                 2            3      0       0.25         0     1          200
+                 1            3      0       0.25         0     1          200];
+
+        slack=1;
+        mkgsfmatrix2 %Program to calculate Generation Shift Factor Matrix
+        nbus=length(busdata(:,1));
+        nd=length(dexdata(:,1));
+        ng=length(gexdata(:,1));
+        prat=linedata(:,7);
+        Ag=zeros(nbus,ng);
+        Ad=zeros(nbus,nd);
+        for i=1:nbus
+            for j=1:ng
+                if gexdata(j,4)==i
+                    Ag(i,j)=1;
+                end
+            end
+        end
+
+        for i=1:nbus
+            for j=1:nd
+                if dexdata(j,4)==i
+                    Ad(i,j)=1;
+                end
+            end
+        end
+
+        f=gexdata(:,2)' 
+        Aeq=[-1 -1] ;
+        Beq=-500;
+
+        A1=ptdf*Ag ;
+
+
+        pd=busdata(:,5);
+        B1=prat(1:3)+ptdf*pd
+        B2=prat(1:3)-ptdf*pd
+        A=[A1; -A1];
+        B=[B1;B2];
+
+        lb=gexdata(:,5) ;
+        ub=gexdata(:,6);
+
+
+
+        [x,fval,exitflag,output,lamda] = linprog(f,A,B,Aeq,Beq,lb,ub)
+        genergy=lamda.eqlin
+        conjcost=(lamda.ineqlin'*[-ptdf ; ptdf])'
+        lmp=genergy+conjcost
+        lineflow=ptdf*(Ag*x-pd)
+
+        
---
+++ test.m
@@ -0,0 +1,63 @@
+% Clearing Workspace and Loading Data
+clear;
+IEEE33bus;
+% Assessing the Data
+nl=length(branchdata(:,1));
+nbus=length(busdata(:,1));
+sn=branchdata(:,1);
+rn=branchdata(:,2);
+r=branchdata(:,3);
+X=branchdata(:,4);
+prat=branchdata(:,6);
+slack=1;
+ns=slack;
+
+y1=zeros(nbus,nbus);
+
+%********************formation of  negative Ybus[Y]------------------
+for i=1:nl
+    y1(sn(i),rn(i))=-1/X(i);
+    y1(rn(i),sn(i))=-1/X(i);
+    y1(sn(i),sn(i))=y1(sn(i),sn(i))+1/X(i);
+    y1(rn(i),rn(i))=y1(rn(i),rn(i))+1/X(i);
+end
+y1;
+y=y1;
+%------------to form Bus Suspectence matrix [B']---------------
+%------shifting of Nth row to corresponding Slack row
+for j=1:nbus
+    y(ns,j)=y(nbus,j);
+end
+%-------shifting of Nth Column to corresponding Slack column-------------
+for i=1:nbus
+    y(i,ns)=y(i,nbus);
+end
+%----removing last row and column we get [B']--------------
+n=nbus-1;
+
+for i=1:n
+    for j=1:n
+        Bp(i,j)=y(i,j);
+    end
+end
+Bp;
+rec=inv(Bp);
+
+xx=zeros(nl,nl);
+node=zeros(nl,nbus);
+for i=1:nl
+    xx(i,i)=1/X(i);
+    sn(i,1)=branchdata(i,1);
+    rn(i,1)=branchdata(i,2);
+    node(i,sn(i,1))=1;
+    node(i,rn(i,1))=-1;
+end
+node1=node;
+node(:,ns)=node(:,nbus);
+node(:,nbus)=[];
+
+sf=xx*node*rec;
+sf=[sf zeros(nl,1)];
+sf(:,nbus)=sf(:,ns);
+sf(:,ns)=zeros;
+sf;
