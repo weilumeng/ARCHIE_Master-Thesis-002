ad6f648a94bc11022b8fe520e63de7cef7f3fb60
gduessel <60701948+gduessel@users.noreply.github.com>
2020-03-08 18:47:03 +0100

    Adding files that use the gurobi solver

---
+++ DCOPF_Gurobi.m
@@ -0,0 +1,43 @@
+clear;
+IEEE33bus;
+slack=1;
+mkgsfmatrix2 %calculate Generation Shift Factor Matrix
+        
+       
+nd=length(busdata(:,3));
+ng=length(gendata(:,1));
+        
+Ag=zeros(nbus,ng);
+
+for i=1:nbus
+    for j=1:ng
+        if gendata(j,1)==i
+            Ag(i,j)=1;
+        end
+    end
+end
+
+
+f=gencostdata(:,6)';
+Aeq=-ones(1,length(gendata(:,1)));
+beq=-sum(busdata(:,3));
+
+A1=ptdf*Ag;
+       
+pd=busdata(:,3);
+        
+B1=prat(1:length(branchdata(:,1)))+ptdf*pd;
+B2=prat(1:length(branchdata(:,1)))-ptdf*pd;
+A=[A1; -A1];
+b=[B1;B2];
+
+lb=gendata(:,10);
+ub=gendata(:,9);
+
+[x,fval,exitflag,output,lambda] = gurobi_linprog(f,A,b,Aeq,beq,lb,ub)
+
+
+genergy=lambda.eqlin
+conjcost=(lambda.ineqlin'*[-ptdf ; ptdf])'
+lmp=genergy+conjcost
+lineflow=ptdf*(Ag*x-pd)
