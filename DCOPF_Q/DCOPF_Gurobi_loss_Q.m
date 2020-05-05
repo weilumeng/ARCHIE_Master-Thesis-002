1c7c214b81d3caa3f82377fdd99a72d85bd1a3a1
gduessel <60701948+gduessel@users.noreply.github.com>
2020-05-05 17:28:02 +0200

    reactive power formulation (added shunt elements - not sure if they work)

--- DCOPF_Q/DCOPF_Gurobi_loss_Q.m
+++ DCOPF_Q/DCOPF_Gurobi_loss_Q.m
@@ -1,7 +1,7 @@
 %% Case Data
 clear;
 tic
-casedata='IEEE33bus.m';
+casedata='IEEE30bus.m';
 run(fullfile(casedata))
 
 %% Loadcurve
@@ -53,7 +53,7 @@ mismatchdispatch=ones(ng,maxiter);
 %% Setting up Matrices for the Quadratic Programming Solver
 % Cost function to minimize
 p_cost=gencostdata(:,6)';
-q_cost=gencostdata(:,6)'; %% for now same costs as active power
+q_cost=0.0*gencostdata(:,6)'; %% for now 10% of active power costs
 
 %% Iteration for the loss formulation starts here
 
@@ -69,18 +69,18 @@ for i=1:nb
     end
 end
 
-% Demand Side of Pg=Pd Equation with Delivery Factor
+% Demand with Delivery Factor
 p_demand=ones(1,nb)*(busdata(:,3).*DF_est);
-q_demand=ones(1,nb)*busdata(:,4);
-%Demand Side of Pg=Pd Equation + Losses
-p_demand=p_demand-Ploss_est;                                      
+q_demand=ones(1,nb)*(busdata(:,4).*DF_est);
+% Demand + Losses
+p_demand=p_demand-Ploss_est;
+q_demand=q_demand-Qloss_est;
 
-
-%% Setting up Matrices for GSF_PP Formulation
+%% Setting up Matrices for GSF Formulation
 A1=GSF_PP*Ag;
 A2=GSF_PQ*Ag;
 pd=busdata(:,3);
-qd=busdata(:,3);
+qd=busdata(:,4);
 
 b1=prat(1:length(branchdata(:,1)))+GSF_PP*(pd+E_est)+GSF_PQ*(qd);
 b2=prat(1:length(branchdata(:,1)))-GSF_PP*(pd+E_est)-GSF_PQ*(qd);
@@ -96,36 +96,37 @@ p_ub=gendata(:,9);
 q_lb=gendata(:,5);
 q_ub=gendata(:,4);
 
-p_lb=[];
-p_ub=[];
 
 %% Quadratic cost functions for each generator
 Qmatrix=zeros(size(A_P,2),size(A_P,2));
 for i=1:length(gencostdata(:,5))
-    if gencostdata(i,5)~=0
-        Qmatrix(i,i)=gencostdata(i,5);
-    end
+    Qmatrix(i,i)=gencostdata(i,5);
+end
+for k=length(gencostdata(:,5))+1:2*length(gencostdata(:,5))
+    Qmatrix(k,k)=q_cost(k-length(gencostdata(:,5)));
 end
 
-Qmatrix=[0 0; 0 0.1*q_cost];
 %% Quadratic Programming with Gurobi Solver
-%names={num2str(zeros(1,length(gendata(:,1))))};
-%for i=1:length(gendata(:,1))
-%names(i) = {num2str("P"+num2str(i))};
-%end
-%model.varnames = names;
-model.obj = [p_cost; 0]; 
+names={num2str(zeros(1,2*length(gendata(:,1))))};
+for i=1:length(gendata(:,1))
+names(i) = {num2str("P"+num2str(i))};
+end
+for k=length(gendata(:,1))+1:2*length(gendata(:,1))
+    names(k) = {num2str("Q"+num2str(k-length(gendata(:,1))))};
+end
+model.varnames = names;
+model.obj = [p_cost zeros(1,length(p_cost))];
 model.Q = sparse(Qmatrix);
-model.A = [sparse(A_P) sparse(A_Q);sparse(Aeq) sparse(0); sparse(0) sparse(Aeq)]; % A must be sparse
+model.A = [sparse(A_P) sparse(A_Q);sparse(Aeq) sparse(zeros(1,size(Aeq,2))); sparse(zeros(1,size(Aeq,2))) sparse(Aeq)]; % A must be sparse
 model.sense = [repmat('<',size(A_P,1),1); repmat('=',size(Aeq,1),1); repmat('=',size(Aeq,1),1)];
 model.rhs = full([b(:);p_demand(:);q_demand(:)]); % rhs must be dense
 if ~isempty(p_lb)
-    model.lb = p_lb;
+    model.lb = [p_lb; q_lb];
 else
     model.lb = -inf(size(model.A,2),1); % default lb for MATLAB is -inf
 end
 if ~isempty(p_ub)
-    model.ub = p_ub;
+    model.ub = [p_ub; q_ub];
 else
     model.ub= inf(size(model.A,2),1);
 end
@@ -144,19 +145,27 @@ lambda.eqlin = results.pi((size(A_P,1)+1):end);
 
 %% Generation Cost, Congestion Cost and LMP for each bus
 generationcost=lambda.eqlin;
-congestioncost=(lambda.ineqlin'*[-GSF_PP ; GSF_PP])';
-lmp=generationcost(1)+congestioncost;
+p_congestioncost=(lambda.ineqlin'*[-GSF_PP ; GSF_PP])';
+q_congestioncost=(lambda.ineqlin'*[-GSF_PQ ; GSF_PQ])';
+p_lmp=generationcost(1)+p_congestioncost;
+q_lmp=generationcost(2)+q_congestioncost;
 
 %% Saving the generation dispatch results at each iteration
 for i=iter:iter
-gendispatch(:,i)=results.x(1);
+gendispatch(:,i)=results.x(1:ng);
 end
 if iter~=1
 mismatchdispatch=(gendispatch(:,iter-1)-gendispatch(:,iter)).^2;
 end
 
 %% Net injections
-pn=Ag*results.x(1)-pd-E_est;
+pn=Ag*results.x(1:ng)-pd-E_est;
+qn=Ag*results.x(ng+1:end)-qd;
+
+
+%V1=Xmat(6,1:5)*pn./baseMVA+Xmat(6,6:end)*qn./baseMVA;
+%V2=Xmat(7,1:5)*pn./baseMVA+Xmat(7,6:end)*qn./baseMVA;
+%V3=Xmat(36,1:33)*pn;
 
 %% Damping parameter (necessary for >100 busses)
 w=0.0;
@@ -166,7 +175,9 @@ pn=w*pn_old+(1-w)*pn;
 end
 
 %% Lineflow based on GSF_PP and net injections
-P_lineflow=GSF_PP*pn;
+P_lineflow=GSF_PP*pn+GSF_PQ*qn;
+Q_lineflow=GSF_QQ*qn+GSF_QP*pn;
+
 if iter>1
 lineflow_old=GSF_PP*pn_old;
 P_lineflow=w*lineflow_old+(1-w)*P_lineflow;
@@ -181,7 +192,7 @@ checkiter=iter;
 %% Adding loss costs to LMPs
 if iter~=1
 losscost=lambda.eqlin*(DF_est-1);
-lmp=lmp+losscost;
+p_lmp=p_lmp+losscost;
 end
 
 %% Criterion to stop further iterations
@@ -203,22 +214,20 @@ for v=1:length(names)
 fprintf('\n %s = %2.2f MW\n', names{v}, results.x(v));
 end
 
-for v=1:length(generationcost)
 fprintf('\n Generation cost = %g $/MWh\n\n', generationcost);
-end
 
 busnumber=busdata(:,1);
 
-for v=1:length(congestioncost)
-    if congestioncost(v)~=0
-    fprintf(' Congestion Cost @ Bus %g = %4.2f $/MWh\n', busnumber(v) , congestioncost(v));
+for v=1:length(p_congestioncost)
+    if p_congestioncost(v)~=0
+    fprintf(' Congestion Cost @ Bus %g = %4.2f $/MWh\n', busnumber(v) , p_congestioncost(v));
     end
 end
 
 fprintf('\n\n   LMP -- Bus Number ||    Generation Cost ||  Congestion Cost \n \n')
-for v=1:length(lmp)
-        if lmp(v)~=0
-         fprintf(' The LMP @ Bus %2.4g is %4.2f $/MWh generation cost and %4.2f $/MWh congestion cost\n', busnumber(v) , generationcost, congestioncost(v));
+for v=1:length(p_lmp)
+        if p_lmp(v)~=0
+         fprintf(' The LMP @ Bus %2.4g is %4.2f $/MWh generation cost and %4.2f $/MWh congestion cost\n', busnumber(v) , generationcost, p_congestioncost(v));
         end
 end
 
--- DCOPF_Q/ptdf_matrix_Q.m
+++ DCOPF_Q/ptdf_matrix_Q.m
@@ -9,7 +9,11 @@ tb=branchdata(:,2);                 %To Bus ...
 G= status ./ branchdata(:,3);       %Conductance of the line
 B= status ./ branchdata(:,4);       %Susceptance of the line
 Bc = status .* branchdata(:, 5);    %total line charging susceptance
+Gs = busdata(:,5);
+Bs = busdata(:,6);
 prat=branchdata(:,6);               %Rated line flow limit
+vmin=busdata(:,13);                 %max voltage in p.u.
+vmax=busdata(:,12);                 %min voltage in p.u.
 slack = find(busdata(:, 2) == 3);   %Finding the reference bus
 
 
@@ -19,8 +23,12 @@ i=find(branchdata(:,9));
 tap(i)=branchdata(i,9);
 B= B ./ tap;
 G= G ./ tap;
-
-
+                                                    
+for i=1:length(branchdata(:,1))                              
+    if G(i)==Inf
+        G(i)=0;
+    end
+end
 %% PTDF Matrix Construction
 
 iline = [(1:nl)'; (1:nl)'];
@@ -30,6 +38,7 @@ Bbus = Cft' * Bf;
 noref   = (2:nb)';      %% use bus 1 for voltage angle reference
 noslack = find((1:nb)' ~= slack);
 noslack2= find((1:(2*nb))' ~= slack);
+
 %% recommended version
 PTDF = zeros(nl, nb);
 PTDF(:, noslack) = full(Bf(:, noref) / Bbus(noslack, noref));
@@ -39,27 +48,50 @@ PTDF(:, noslack) = full(Bf(:, noref) / Bbus(noslack, noref));
 Bf_shunt = sparse(iline, [fb; tb], [B; -B], nl, nb);
 Bbus_shunt = Cft' * Bf_shunt;
 
+%Adding shunts (not sure if thats correct)
+for i=1:nb
+    for j=1:nb
+        if i==branchdata(j,1)
+            if branchdata(j,5)~=0
+                Bbus_shunt(i,i)=Bbus_shunt(i,i)+(1/(branchdata(j,5)/2));
+           end
+        end
+    end
+end
+
 Gf = sparse(iline, [fb; tb], [G; -G], nl, nb);
 Gbus = Cft' * Gf;
 
 Gf_shunt = sparse(iline, [fb; tb], [G; -G], nl, nb);
 Gbus_shunt = Cft' * Gf_shunt;
 
+% Adding shunts (not sure if thats correct)
+for i=1:nb
+    for j=1:nb
+        if i==branchdata(j,1)
+            if branchdata(j,5)~=0
+                Gbus_shunt(i,i)=Gbus_shunt(i,i)+(1/(branchdata(j,5)/2));
+            end
+        end
+    end
+end
+
 %% Constructing the Inverse of C
-C=-[Bbus -Gbus;Gbus Bbus];
+C=[Bbus -Gbus_shunt;Gbus Bbus_shunt];
+
 
 % inverted C --> Xmat filled with zeros at slack bus row and column
+
 Xmat=zeros(2*nb,2*nb);
-Xmat(noslack2,noslack2)=full(C(noslack2,noslack2)\eye(2*nb-1));
+Xmat(noslack2,noslack2)=-(C(noslack2,noslack2)\eye(2*nb-1));
 
 
 %% Constructing new partial GSF matrices 
 
-GSF_PP = full(Gf * Xmat(nb+1:end,1:nb))+full(-Bf * Xmat(1:nb,1:nb));
-GSF_PQ = full(Gf * Xmat(nb+1:end,nb+1:end))+full(-Bf * Xmat(1:nb,nb+1:end));
-GSF_QP = full(-Gf * Xmat(1:nb,1:nb))+full(-Bf * Xmat(nb+1:end,1:nb));
-GSF_QQ = full(-Gf * Xmat(1:nb,nb+1:end))+ full(-Bf * Xmat(nb+1:end,nb+1:end));
-
+GSF_PP =  full(Gf * Xmat(nb+1:end,1:nb))    -full(Bf * Xmat(1:nb,1:nb));
+GSF_PQ =  full(Gf * Xmat(nb+1:end,nb+1:end))-full(Bf * Xmat(1:nb,nb+1:end));
+GSF_QP = -full(Gf * Xmat(1:nb,1:nb))        -full(Bf * Xmat(nb+1:end,1:nb));
+GSF_QQ = -full(Gf * Xmat(1:nb,nb+1:end))    -full(Bf * Xmat(nb+1:end,nb+1:end));
 
 
 %Checking for unrestricted line flows - if line flow at line i is  
@@ -70,3 +102,8 @@ for i=1:length(prat)
         prat(i)=Inf;
     end
 end
+
+
+
+G= status ./ branchdata(:,3);
+G= G ./ tap;
